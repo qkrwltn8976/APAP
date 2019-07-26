f48ac0115125356ca4e85f22a70efd00d03f592b
Jungsoo <bjsoo1123@naver.com>
2019-07-27 02:44:25 +0900

    revert

--- APAP/main/urls.py
+++ APAP/main/urls.py
@@ -4,8 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+<<<<<<< HEAD
 	path('home/', views.home, name="home"),
 	path('<username>/request', views.request, name="request"),
 	path('<username>/request/<int:id>', views.detail, name="detail"),
 	path('<username>/mypage', views.mypage, name="mypage"),
+=======
+	path('', views.home, name="home"),
+>>>>>>> parent of f6b5f74... added apps
 ] 
