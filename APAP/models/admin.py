0ec5714da6d40ce59965e0989354cb680f87328d
jisu park <qkrwltn8976@naver.com>
2019-07-28 23:23:57 +0900

    created new models
    university & lecture

--- APAP/models/admin.py
+++ APAP/models/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from models.models import User
+from models.models import User, University, Lecture
 
 admin.site.register(User)
+admin.site.register(University)
+admin.site.register(Lecture)
 # Register your models here.
--- APAP/models/models.py
+++ APAP/models/models.py
@@ -9,27 +9,19 @@ class User(AbstractUser):
 	level = models.IntegerField(default=1)
 	verified = models.BooleanField(default=False)
 
-# class University():
-# 	name = models.CharField(max_length=100)
+class University(models.Model):
+	name = models.CharField(max_length=100)
 	
-# class Major():
-# 	name = models.CharField(max_length=100)
+class Lecture(models.Model):
+	name = models.CharField(max_length=100)
+	code = models.IntegerField()
+	day_time = models.CharField(max_length=100)
 
-
-# class Subject():
-# 	name = models.CharField(max_length=100)
-# 	code = models.IntegerField()
-# 	classroom = models.CharField(max_length=100)
-# 	major = models.ForeignKey(
-# 		Major,
-# 		on_delete = models.CASCADE,
-# 		related_name = 'major',
-# 	)
-# 	university = models.ForeignKey(
-# 		University,
-# 		on_delete = models.CASCADE,
-# 		related_name = 'university',
-# 	)
+	university = models.ForeignKey(
+		University,
+		on_delete = models.CASCADE,
+		related_name = 'university',
+	)
 
 
 
