01189aadc60936b7bbcd0e8c5f032ea90fae57b5
Jungsoo <bjsoo1123@naver.com>
2019-08-05 14:56:49 +0900

    JustDoit

--- APAP/authservice/urls.py
+++ APAP/authservice/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 from . import views
 
 app_name = "authservice"
-
 urlpatterns = [
 	path('', views.signin, name="signin"),
 	path('register/', views.register, name="register"),
 	path('signup/', views.signup, name="signup"),
 	path('login/', views.login, name="login"),
+	path('index/', views.index, name="index"),
 ]  
